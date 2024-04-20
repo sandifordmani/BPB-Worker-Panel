f53b2f8646809231644682b7eba40b788eec1e80
Dragon <154963456+sandifordmani@users.noreply.github.com>
2024-04-20 11:14:47 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -316,7 +316,7 @@ async function vlessOverWSHandler(request) {
 			const {
 				hasError,
 				message,
-				portRemote = 443,
+				portRemote = [443, 8443, 2053, 2083, 2087, 2096, 80, 8080, 8880, 2052, 2082, 2086, 2095],
 				addressRemote = '',
 				rawDataIndex,
 				vlessVersion = new Uint8Array([0, 0]),
